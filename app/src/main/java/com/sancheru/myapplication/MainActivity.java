6b0823c2b0c351c8e8334c1c491c3b44327cb27f
Sandeep Cherukuri <sandeep.cherukuri@sapientrazorfish.com>
2020-07-01 10:50:24 -0500

    first commit

---
+++ app/src/main/java/com/sancheru/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sancheru.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

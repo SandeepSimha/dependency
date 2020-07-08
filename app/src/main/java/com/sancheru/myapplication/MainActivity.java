23dd801da330c5d7e0a05c2cd1ce0b845a4e7c6b
Sandeep Cherukuri <sandeep.cherukuri@sapientrazorfish.com>
2020-07-08 09:07:37 -0500

    Adding builds

--- app/src/main/java/com/sancheru/myapplication/MainActivity.java
+++ app/src/main/java/com/sancheru/myapplication/MainActivity.java
@@ -3,13 +3,18 @@ package com.sancheru.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+//import com.google.analytics.tracking.android.GoogleAnalytics;
+
+
 import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
